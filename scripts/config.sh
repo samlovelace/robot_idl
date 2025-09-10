01dc490db5bef7eaa111be9fdd4e05eac042c2a9
samlovelace <samlovelace12@gmail.com>
2025-09-10 19:28:47 -0400

    updates config and setup scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,15 +1,18 @@
-
 #!/bin/bash
 
-REPO_BASE_URL="https://github.com/samlovelace"
-WORKSPACE_DIR=~/robot_ws
-REPOS=("perception")
+REPO_BASE_URL="git@github.com:samlovelace"
+WORKSPACE_DIR=/home/sam/dev/cpp/test_ws
 
 # Declare associative array of modules
 declare -A MODULES
-
-# Each key is a module, value is space-separated list of repos
 MODULES[autonomy]="neo cortex"
 MODULES[manipulation]="arm_configs arm"
 MODULES[perception]="vision"
 MODULES[vehicle]="abv_controller"
+
+# Use passed arguments or default to all keys
+if [ "$#" -eq 0 ]; then
+    REPOS=("${!MODULES[@]}")
+else
+    REPOS=("$@")
+fi
